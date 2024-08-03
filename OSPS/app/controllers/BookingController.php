dd7570d40bf241a81432ca73d18cf6e87322f362
hvuuuu <104177995@student.swin.edu.au>
2024-08-03 14:22:54 +0700

    test v2.0

--- OSPS/app/controllers/BookingController.php
+++ OSPS/app/controllers/BookingController.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '../core/Database.php';
-require_once __DIR__ . '../models/Reservation.php';
+require_once __DIR__ . '/../core/Database.php';
+require_once __DIR__ . '/../models/Reservation.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'book') {
     $slotId = $_POST['slot_id'];
